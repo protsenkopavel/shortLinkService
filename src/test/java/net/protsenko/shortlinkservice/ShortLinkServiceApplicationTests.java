5ad37822f8a96fe0922dddf1a869e8ba50d3b905
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-03-09 16:29:13 +0300

    Project init

---
+++ src/test/java/net/protsenko/shortlinkservice/ShortLinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.protsenko.shortlinkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortLinkServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
